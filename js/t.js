65a134bc35c06dbad227a287e0c6cf201ee98b10
Millenadv <millenasferraroni@gmail.com>
2018-08-30 22:49:06 -0300

    segundaAula

---
+++ js/t.js
@@ -0,0 +1,20 @@
+// Escrever os números de 0 a 10 com o laço de repetição
+
+//Escrever a tabuada do 2
+
+var i;
+for (i=0; i <= 10; i++) {
+	document.write("2 x " + i + "= " + (i*2) + "<br>" );
+
+}
+   document.write("<br>"); 
+
+var j;
+var t;
+for (j=3; j <= 10; j++) {
+	for (t=0; t <= 10; t++) {
+		document.write(j + " x " + t + " = " + (j*t) + "<br>" );
+
+	}
+	document.write("<br>" );
+}
