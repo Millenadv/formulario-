8727ad4ae79cda93eb8f289da947d4bbad9eeb60
Millenadv <millenasferraroni@gmail.com>
2018-08-30 22:01:50 -0300

    formulario

---
+++ js/formulario.js
@@ -0,0 +1,13 @@
+// Função da data de nascimento 
+function ano () {
+	var data_nasc = document.getElementById("data_nasc");
+	var i;
+	for(i = 2018; i >= 1920; i--){
+		var opt0 = document.createElement("option");
+		opt0.value = i;
+		opt0.text = i;
+		data_nasc.add(opt0, data_nasc.options[0]);
+	}
+}
+
+alert ("Seja bem vindo!")
